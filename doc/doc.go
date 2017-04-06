69bc5b875aa9b0edbd06c2ad91582cde1201ee1e
Gavin Williams <fatmcgav@gmail.com>
2017-04-06 09:38:12 +0100

    Add support for reading `description` tag from `output` resources. Fixes #24

--- doc/doc.go
+++ doc/doc.go
@@ -117,9 +117,13 @@ func outputs(list *ast.ObjectList) []Output {
 		if is(item, "output") {
 			name, _ := strconv.Unquote(item.Keys[1].Token.Text)
 
+			items := item.Val.(*ast.ObjectType).List.Items
 			var desc string
-			if c := item.LeadComment; c != nil {
-				desc = comment(c.List)
+			switch {
+			case description(items) != "":
+				desc = description(items)
+			case item.LeadComment != nil:
+				desc = comment(item.LeadComment.List)
 			}
 
 			ret = append(ret, Output{
