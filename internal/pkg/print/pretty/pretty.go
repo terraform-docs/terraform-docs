864ea8ddbc4cdef7062b5e23299c792bbf3e2fcb
Khosrow Moossavi <khos2ow@gmail.com>
2019-12-18 16:11:52 -0500

    Adjust 'pkg/print/pretty' based on moarta's changes

--- internal/pkg/print/pretty/pretty.go
+++ internal/pkg/print/pretty/pretty.go
@@ -3,20 +3,32 @@ package pretty
 import (
 	"bytes"
 	"fmt"
+	"strings"
 
 	"github.com/segmentio/terraform-docs/internal/pkg/doc"
 	"github.com/segmentio/terraform-docs/internal/pkg/print"
-	"github.com/segmentio/terraform-docs/internal/pkg/print/markdown"
 )
 
 // Print prints a pretty document.
 func Print(document *doc.Doc, settings *print.Settings) (string, error) {
 	var buffer bytes.Buffer
 
+	if settings.SortByName {
+		if settings.SortInputsByRequired {
+			doc.SortInputsByRequired(document.Inputs)
+		} else {
+			doc.SortInputsByName(document.Inputs)
+		}
+	}
+
+	if settings.SortByName {
+		doc.SortOutputsByName(document.Outputs)
+	}
+
 	printInputs(&buffer, document.Inputs, settings)
 	printOutputs(&buffer, document.Outputs, settings)
 
-	return markdown.Sanitize(buffer.String()), nil
+	return buffer.String(), nil
 }
 
 func getInputDefaultValue(input *doc.Input, settings *print.Settings) string {
@@ -29,17 +41,29 @@ func getInputDefaultValue(input *doc.Input, settings *print.Settings) string {
 	return result
 }
 
+func getDescription(description string) string {
+	var result = "no description"
+
+	if description != "" {
+		result = strings.TrimSuffix(description, "\n")
+	}
+
+	return result
+}
+
 func printInputs(buffer *bytes.Buffer, inputs []doc.Input, settings *print.Settings) {
-	buffer.WriteString("\n")
+	buffer.WriteString("\n\n")
 
 	for _, input := range inputs {
-		format := "  \033[36mvar.%s\033[0m (%s)\n  \033[90m%s\033[0m\n\n"
+		format := "\033[36minput.%s\033[0m (%s)\n\033[90m%s\033[0m\n\n"
 		buffer.WriteString(
 			fmt.Sprintf(
 				format,
 				input.Name,
 				getInputDefaultValue(&input, settings),
-				input.Description))
+				getDescription(input.Description),
+			),
+		)
 	}
 
 	buffer.WriteString("\n")
@@ -49,14 +73,13 @@ func printOutputs(buffer *bytes.Buffer, outputs []doc.Output, settings *print.Se
 	buffer.WriteString("\n")
 
 	for _, output := range outputs {
-		format := "  \033[36moutput.%s\033[0m\n  \033[90m%s\033[0m\n\n"
-
+		format := "\033[36moutput.%s\033[0m\n\033[90m%s\033[0m\n\n"
 		buffer.WriteString(
 			fmt.Sprintf(
 				format,
 				output.Name,
-				output.Description))
+				getDescription(output.Description),
+			),
+		)
 	}
-
-	buffer.WriteString("\n")
 }
