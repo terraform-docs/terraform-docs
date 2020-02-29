1e5034a402ecfba21ae72ad76cf86cfea87653fd
Khosrow Moossavi <khos2ow@gmail.com>
2020-02-29 15:22:50 -0500

    improve internal/types coverage

--- internal/types/empty_test.go
+++ internal/types/empty_test.go
@@ -2,6 +2,8 @@ package types
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestEmpty(t *testing.T) {
@@ -28,4 +30,50 @@ func TestEmpty(t *testing.T) {
 			},
 		},
 	})
+}
+
+func TestEmptyUnderlying(t *testing.T) {
+	tests := []struct {
+		name  string
+		value string
+	}{
+		{
+			name:  "empty underlying",
+			value: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert := assert.New(t)
+			assert.Equal(tt.value, Empty(tt.value).underlying())
+		})
+	}
+}
+
+func TestEmptyMarshalJSON(t *testing.T) {
+	tests := []struct {
+		name     string
+		value    string
+		expected string
+	}{
+		{
+			name:     "string marshal JSON",
+			value:    "foo",
+			expected: "\"\"",
+		},
+		{
+			name:     "string marshal JSON",
+			value:    "",
+			expected: "\"\"",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert := assert.New(t)
+			actual, err := Empty(tt.value).MarshalJSON()
+
+			assert.Nil(err)
+			assert.Equal(tt.expected, string(actual))
+		})
+	}
 }
--- internal/types/list_test.go
+++ internal/types/list_test.go
@@ -1,7 +1,11 @@
 package types
 
 import (
+	"bytes"
+	"encoding/xml"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestList(t *testing.T) {
@@ -33,4 +37,86 @@ func TestList(t *testing.T) {
 			},
 		},
 	})
+}
+
+func TestListUnderlying(t *testing.T) {
+	tests := []struct {
+		name  string
+		value []interface{}
+	}{
+		{
+			name:  "list underlying",
+			value: List{"foo", "bar", "baz"},
+		},
+		{
+			name:  "list underlying",
+			value: List{"1", "2", "3"},
+		},
+		{
+			name:  "list underlying",
+			value: List{true, false, true},
+		},
+		{
+			name:  "list underlying",
+			value: List{10, float64(1000), int8(42)},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert := assert.New(t)
+			assert.Equal(tt.value, List(tt.value).Underlying())
+		})
+	}
+}
+
+func TestListMarshalXML(t *testing.T) {
+	tests := []struct {
+		name     string
+		value    []interface{}
+		expected string
+	}{
+		{
+			name:     "list marshal XML",
+			value:    List{"foo", "bar", "baz"},
+			expected: "<test><item>foo</item><item>bar</item><item>baz</item></test>",
+		},
+		{
+			name:     "list marshal XML",
+			value:    List{"1", "2", "3"},
+			expected: "<test><item>1</item><item>2</item><item>3</item></test>",
+		},
+		{
+			name:     "list marshal XML",
+			value:    List{true, false, true},
+			expected: "<test><item>true</item><item>false</item><item>true</item></test>",
+		},
+		{
+			name:     "list marshal XML",
+			value:    List{10, float64(1000), int8(42)},
+			expected: "<test><item>10</item><item>1000</item><item>42</item></test>",
+		},
+		{
+			name:     "list marshal XML",
+			value:    List{},
+			expected: "<test></test>",
+		},
+		{
+			name:     "list marshal XML",
+			value:    List{nil, "something"},
+			expected: "<test><item></item><item>something</item></test>",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert := assert.New(t)
+			var b bytes.Buffer
+			encoder := xml.NewEncoder(&b)
+			start := xml.StartElement{Name: xml.Name{Local: "test"}}
+			err := List(tt.value).MarshalXML(encoder, start)
+
+			encoder.Flush()
+			assert.Nil(err)
+			assert.Equal(tt.expected, b.String())
+		})
+	}
 }
