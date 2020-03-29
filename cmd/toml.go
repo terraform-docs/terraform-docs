d9eed011b7566910f16c228779fddfb8fe04b47b
Khosrow Moossavi <khos2ow@gmail.com>
2020-03-29 18:23:22 -0400

    Add kind:formatter to toml command

--- cmd/toml.go
+++ cmd/toml.go
@@ -9,6 +9,9 @@ var tomlCmd = &cobra.Command{
 	Args:  cobra.ExactArgs(1),
 	Use:   "toml [PATH]",
 	Short: "Generate TOML of inputs and outputs",
+	Annotations: map[string]string{
+		"kind": "formatter",
+	},
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return doPrint(args[0], format.NewTOML(settings))
 	},
