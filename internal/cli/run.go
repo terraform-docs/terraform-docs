db29f2cf5c1f06645ed518953215777967261b90
Khosrow Moossavi <khos2ow@gmail.com>
2020-05-24 22:07:58 -0400

    add implementation for new flags

--- internal/cli/run.go
+++ internal/cli/run.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 
 	"github.com/segmentio/terraform-docs/internal/format"
 	"github.com/segmentio/terraform-docs/internal/module"
@@ -14,7 +15,17 @@ import (
 // flags and arguments passed through CLI execution.
 func PreRunEFunc(config *Config) func(*cobra.Command, []string) error {
 	return func(cmd *cobra.Command, args []string) error {
-		return normalize(cmd.CommandPath(), cmd.Flags(), config)
+		cmd.Flags().VisitAll(func(f *pflag.Flag) {
+			changedfs[f.Name] = f.Changed
+		})
+
+		config.normalize(cmd.CommandPath())
+
+		if err := config.validate(); err != nil {
+			return err
+		}
+
+		return nil
 	}
 }
 
@@ -24,24 +35,28 @@ func PreRunEFunc(config *Config) func(*cobra.Command, []string) error {
 // initializes required print.Format instance and executes it.
 func RunEFunc(config *Config) func(*cobra.Command, []string) error {
 	return func(cmd *cobra.Command, args []string) error {
-		settings, options, err := config.extract()
-		if err != nil {
-			return err
-		}
+		settings, options := config.extract()
+
 		printer, err := format.Factory(config.Formatter, settings)
 		if err != nil {
 			return err
 		}
+
 		options.Path = args[0]
+
 		tfmodule, err := module.LoadWithOptions(options)
 		if err != nil {
 			return err
+
 		}
+
 		output, err := printer.Print(tfmodule, settings)
 		if err != nil {
 			return err
 		}
+
 		fmt.Println(output)
+
 		return nil
 	}
 }
