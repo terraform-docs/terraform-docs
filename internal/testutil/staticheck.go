9203104476a5a50336f94376f8fa378787f316a9
Khosrow Moossavi <khos2ow@gmail.com>
2021-02-23 16:55:16 -0500

    Upgrade staticcheck to v0.1.2
    Signed-off-by: Khosrow Moossavi <khos2ow@gmail.com>

---
+++ internal/testutil/staticheck.go
@@ -0,0 +1,15 @@
+/*
+Copyright 2021 The terraform-docs Authors.
+
+Licensed under the MIT license (the "License"); you may not
+use this file except in compliance with the License.
+
+You may obtain a copy of the License at the LICENSE file in
+the root directory of this source tree.
+*/
+
+package testutil
+
+import (
+	_ "honnef.co/go/tools/staticcheck" //nolint
+)
