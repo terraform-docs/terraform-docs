4397facaffecb502c9780dd520c602c39d40783d
Khosrow Moossavi <khos2ow@gmail.com>
2021-04-30 19:53:29 -0400

    Validate license header on files on CI
    Signed-off-by: Khosrow Moossavi <khos2ow@gmail.com>

--- internal/plugin/plugin.go
+++ internal/plugin/plugin.go
@@ -1,3 +1,13 @@
+/*
+Copyright 2021 The terraform-docs Authors.
+
+Licensed under the MIT license (the "License"); you may not
+use this file except in compliance with the License.
+
+You may obtain a copy of the License at the LICENSE file in
+the root directory of this source tree.
+*/
+
 package plugin
 
 import (
