66370fdea877588f721145e2b329db5e2367945c
Tristan Jahnke <trjahnke@protonmail.com>
2026-05-12 16:19:56 -0500

    ops: Add copyright to top of new files
    Signed-off-by: Tristan Jahnke <trjahnke@protonmail.com>

--- terraform/resource_extract.go
+++ terraform/resource_extract.go
@@ -1,6 +1,20 @@
+/*
+Copyright 2021 The terraform-docs Authors.
+
+Licensed under the MIT license (the "License"); you may not
+use this file except in compliance with the License.
+
+You may obtain a copy of the License at the LICENSE file in
+the root directory of this source tree.
+*/
+
 package terraform
 
-import "github.com/hashicorp/hcl/v2"
+import (
+	"strings"
+
+	"github.com/hashicorp/hcl/v2"
+)
 
 type rawResource struct {
 	Mode          string // "managed" || "data"
@@ -14,6 +28,14 @@ type rawResource struct {
 
 func extractResources(files map[string]*hcl.File) []rawResource {
 	var out []rawResource
+
+	bodySchema := &hcl.BodySchema{
+		Attributes: []hcl.AttributeSchema{
+			{
+				Name: "provider",
+			},
+		},
+	}
 
 	schema := &hcl.BodySchema{
 		Blocks: []hcl.BlockHeaderSchema{
@@ -32,15 +54,34 @@ func extractResources(files map[string]*hcl.File) []rawResource {
 		content, _, _ := file.Body.PartialContent(schema)
 		for _, block := range content.Blocks {
 			var mode string = "managed"
+			inner, _, _ := block.Body.PartialContent(bodySchema)
+			providerName := strings.SplitN(block.Labels[0], "_", 2)[0]
+			providerAlias := ""
 			if block.Type == "data" {
 				mode = "data"
 			}
+			if attribute, ok := inner.Attributes["provider"]; ok {
+				// provider = aws.useast1 -> Traversal: [aws, useast1]
+				traversal, _ := hcl.AbsTraversalForExpr(attribute.Expr)
+				if len(traversal) >= 1 {
+					if root, ok := traversal[0].(hcl.TraverseRoot); ok {
+						providerName = root.Name
+					}
+				}
+				if len(traversal) >= 2 {
+					if attribute, ok := traversal[1].(hcl.TraverseAttr); ok {
+						providerAlias = attribute.Name
+					}
+				}
+			}
 			out = append(out, rawResource{
-				Mode:     mode,
-				Type:     block.Labels[0],
-				Name:     block.Labels[1],
-				Filename: name,
-				Line:     block.DefRange.Start.Line,
+				Mode:          mode,
+				Type:          block.Labels[0],
+				Name:          block.Labels[1],
+				Filename:      name,
+				Line:          block.DefRange.Start.Line,
+				ProviderName:  providerName,
+				ProviderAlias: providerAlias,
 			})
 		}
 	}
