524266004b275bffec3646d79878b22d116f1754
Richard Quadling <RQuadling@GMail.com>
2026-07-28 10:39:52 +0100

    Test output and documentation

--- examples/variables.tf
+++ examples/variables.tf
@@ -187,3 +187,35 @@ variable "object_default_empty" {
   type    = object({})
   default = {}
 }
+
+variable "variable_with_no_validation" {
+    description = "This variable has no validation"
+    type        = string
+    default     = ""
+}
+
+variable "variable_with_one_validation" {
+    description = "This variable has one validation"
+    type        = string
+    default     = ""
+
+    validation {
+        condition     = (length(var.variable_with_one_validation) == 0 || length(var.variable_with_one_validation) == 10)
+        error_message = "var.variable_with_one_validation must be empty or 10 characters long."
+    }
+}
+
+variable "variable_with_two_validations" {
+    description = "This variable has two validations"
+    type        = string
+
+    validation {
+        condition     = (length(var.variable_with_two_validations) == 10)
+        error_message = "var.variable_with_two_validations must be 10 characters long."
+    }
+
+    validation {
+        condition     = (startswith(var.variable_with_two_validations, "magic"))
+        error_message = "var.variable_with_two_validations must start with 'magic'."
+    }
+}
